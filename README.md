ae03516b97abc60171597ccb4c0e0cf5677ec0fc
qiqiboy <imqiqiboy@gmail.com>
2015-01-15 16:40:42 +0800

    add file

--- README.md
+++ README.md
@@ -1,2 +1,106 @@
 # JS2CSSKeyframes
-Create CSS3 keyframes animation from javascript.
+Create CSS3 keyframes animation from javascript. 
+使用JS动态创建管理CSS3 Animations。
+
+## 为何使用
+随着支持HTML5的普及，页面中使用各种动画的地方越来越多，尤其是一个小型的动画，使用css3来做无疑是既方便又快捷的一个选择。 
+
+但是由于浏览器间差异（主要是 webkit 和 moz），我们目前使用css3动画还必须写上各种浏览器前缀，这样无疑给开发工作增添许多麻烦，添加 修改都是无尽的麻烦。 
+
+JS2CSSKeyframes就是解决这个问题的，它可以自动根据不同浏览器生成其支持的css3动画@keyframes，省去各种前缀。动画中的css3属性也会自动加入前缀，无需单独书写。 
+
+例如：new JS2CSSKeyframes("test", {from:{transform:'translate(0,0);'},to:{transform:'translate(100px,100px);'}}); 
+
+在不同浏览器下将会生成以下三种之一
+*@-webkit-keyframes test { from { -webkit-transform:translate(0,0) } to { -webkit-transform:translate(100px,100px) } } 
+*@-moz-keyframes test { from { -moz-transform:translate(0,0) } to { -moz-transform:translate(100px,100px) } } 
+*@keyframes test { from { transform:translate(0,0) } to { transform:translate(100px,100px) } } 
+
+
+## 如何使用
+```javascript
+
+// @Class JS2CSSKeyframes
+// @param String name
+// @param Object config 初始画板配置
+//                      .width int 画板宽度
+//                      .height int 画板高度
+//                      .lineWidth int 笔画宽度
+//                      .color 笔画颜色
+//                      .bgcolor 背景颜色
+/* @description 
+ * @Class JS2CSSKeyframes
+ * @param String name 名称，可省略该参数，name将随机生成
+ * @param String|Object|Array config 动画帧设定
+ *
+ * @example-1 new JS2CSSKeyframes("test", {
+ *              "from":"width:300px;height:100px;",
+ *              "50%":"width:30px;height:10px;",
+ *              "to":{width:"300px",height:"10px"}
+ *           });
+ *
+ * @example-2 new JS2CSSKeyframes("test1", ["width:300px;height:100px;","width:30px;height:10px;"]);
+ *
+ * @example-3 new JS2CSSKeyframes("test2", "from {width:300px;height:100px;} to {width:300px;height:100px;}");
+ *
+ * 注：也可以使用 JS2CSSKeyframes.add(name,config) 来创建动画
+ */
+
+//用法一
+new JS2CSSKeyframes('ani_1',{
+    '0%':{width:0,height:0},
+    '50%':{width:'100px',height:'100%'},
+    '100%':{width:0,height:0}
+});
+
+//用法二
+new JS2CSSKeyframes('ani_1',{
+    '0%':'width:0;height:0',
+    '50%':'width:100px;height:100%;',
+    '100%':'width:0;height:0;'
+});
+
+//用法三
+//均等比例帧可以使用数组省略比例
+new JS2CSSKeyframes('ani_1',[
+    'width:0;height:0', //0%
+    'width:100px;height:100%;', //50%
+    'width:0;height:0;' //100%
+]);
+
+//用法四
+//可以省略名称，name将随机生成，可以通过 .name 来查看生成的名称
+ani=new JS2CSSKeyframes([
+    'width:0;height:0', //0%
+    'width:100px;height:100%;', //50%
+    'width:0;height:0;' //100%
+]);
+console.log(nai.name); //输出 css3Ani_999997712 
+
+
+
+//JS2CSSKeyframes实例对象的其它属性及方法说明
+//@prop String name 动画名称
+//@prop String cssText 动画内容
+//@prop CSSRuleList 动画帧信息集合
+//@prop Object keyframes 动画帧对象，键值为比例百分比，0% 50% 等 
+
+//@method get(key) 获取指定进度的CSSKeyframeRule帧
+//@method add(key,value) 增加进度为key，样式为value的帧
+//@method remove(key) 删除进度为key的帧
+//@method clear() 删除所有的帧
+
+
+
+//JS2CSSKeyframes.CSSKeyframes //获取页面上所有的css3动画
+//JS2CSSKeyframes.get(name) //获取页面上名为name的动画
+//JS2CSSKeyframes.remove(name) //删除页面上名为name的动画
+//JS2CSSKeyframes.add(name,config) //增加动画，同 new JS2CSSKeyframes(name,config);
+//JS2CSSKeyframes.vendor 当前浏览器前缀 -webkit -moz- 或空字符串
+//JS2CSSKeyframes.support 是否支持css动画，不支持css3动画的浏览器中调用JS2CSSKeyframes其它方法将会报错
+
+
+````
+
+## DEMO 
+http://u.boy.im/sketch/css3keyframes
---
+++ src/j2ckf.js
@@ -0,0 +1,226 @@
+/*
+ * @author qiqiboy
+ * @github https://github.com/qiqiboy/JS2CSSKeyframes
+ */
+;
+(function(ROOT, struct, undefined){
+    "use strict";
+
+    var doc=document,
+        slice=[].slice,
+        CSSRule=ROOT.CSSRule||{},
+        IMPORT_RULE=CSSRule.IMPORT_RULE,
+        MEDIA_RULE=CSSRule.MEDIA_RULE,
+        KEYFRAMES_RULE=CSSRule.KEYFRAMES_RULE||CSSRule.WEBKIT_KEYFRAMES_RULE||CSSRule.MOZ_KEYFRAMES_RULE,
+        divstyle=document.documentElement.style,
+        camelCase=function(str){
+            return (str+'').replace(/^-ms-/, 'ms-').replace(/-([a-z]|[0-9])/ig, function(all, letter){
+                return (letter+'').toUpperCase();
+            });
+        },
+        cssVendor=function(){
+            var tests="-webkit- -moz- -o- -ms-".split(" "),
+                prop;
+            while(prop=tests.shift()){
+                if(camelCase(prop+'animation') in divstyle){
+                    return prop;
+                }
+            }
+            return '';
+        }(),
+        KEY_REG=new RegExp('@(?:'+cssVendor+')?keyframes','i');
+
+    function getSheet(){ //获取可以用的样式以用来插入css3 keyframes
+        if(!doc.styleSheets.length){
+            var style=doc.createElement('style');
+            style.rel='stylesheet';
+            style.type='text/css';
+            doc.getElementsByTagName('head')[0].appendChild(style);
+        }
+        
+        return doc.styleSheets.item(0);
+    }
+
+    function iterateSheet(callback){
+        slice.call(doc.styleSheets).forEach(function getRule(sheet){
+            slice.call(sheet.cssRules||styles.rules).forEach(function(rule,i){
+                if(rule.type==IMPORT_RULE){//imported css
+                    getRule(rule.styleSheet||rule.sheet);
+                }else if(rule.type==MEDIA_RULE){
+                    getRule(rule);
+                }else if(rule.type==KEYFRAMES_RULE){
+                    callback(rule,i,sheet);
+                }
+            });
+        });
+    }
+
+    function fixCSS3(name){
+        var prop=camelCase(name),
+            _prop=camelCase(cssVendor+prop);
+        return (prop in divstyle) && name || (_prop in divstyle) && cssVendor+name || name;
+    }
+
+    function fixKey(key){
+        return {from:'0%',to:'100%'}[key]||(key+'').replace('%','')+'%';
+    }
+    
+    function getKeyframesStyle(keys,name){
+        var cssText="";
+        if(typeof keys=='string'){
+            if(KEY_REG.test(keys)){
+                return keys;
+            }
+            cssText=keys.replace(/^\s*{\s*(?={)|}\s*(?=}\s*$)/gi,'');
+        }else if(Array.isArray(keys)){
+            cssText=keys.map(function(rule,i){
+                return (i/(keys.length-1)*100||0)+'%'+' { '+getKeyframesRule(rule)+' }';
+            }).join(' ');
+        }else if(keys){
+            cssText=Object.keys(keys).map(function(key){
+                    return fixKey(key)+' { '+getKeyframesRule(keys[key])+' }';
+                }).join(' ');
+        }
+
+        return '@'+cssVendor+'keyframes '+name+' { '+cssText+' }';;
+    }
+
+    function getKeyframesRule(rule,name){
+        var ruleText="";
+        if(typeof rule=='string'){
+            ruleText=getKeyframesRule(rule.split(/\s*;\s*/g));
+        }else if(Array.isArray(rule)){
+            ruleText=getKeyframesRule(rule.reduce(function(obj,text){
+                var ret=text.split(/\s*:\s*/);
+                if(ret.length>1){
+                    obj[ret[0]]=ret[1];
+                }
+                return obj;
+            },{}));
+        }else if(rule){
+            ruleText=Object.keys(rule).map(function(key){
+                return fixCSS3(key)+': '+rule[key]+';';
+            }).join(' ');
+        }
+
+        return ruleText;
+    }
+
+    struct.prototype={
+        constructor:struct,
+        init:function(name, keys){
+            var ckf; //CSSKeyframes
+            if(typeof name=='object' || KEY_REG.test(name)){
+                if(name.cssRules && name.type==KEYFRAMES_RULE){
+                    ckf=name;
+                }else{
+                    keys=name;
+                    name=null;
+                }
+            }
+
+            if(!name){
+                name='css3Ani_'+parseInt(Math.random()*1e10);
+            }
+
+            if(!ckf){
+                var sheet=getSheet(),
+                    id=sheet.insertRule(getKeyframesStyle(keys,name),sheet.cssRules.length);
+                ckf=sheet.cssRules[id];
+            }
+
+            this.keyframesRule=ckf||{};
+
+            return this.extract();
+        },
+        extract:function(){
+            this.keyframes=slice.call(this.cssRules).reduce(function(obj,rule){
+                obj[rule.keyText]=rule;
+                return obj;
+            },{});
+
+            return this;
+        },
+        get:function(name){
+            return this.keyframes[fixKey(name)];
+        },
+        add:function(name,value){
+            if(typeof name=='object'){
+                for(var key in name){
+                    this.add(key, name[key]);
+                }
+            }else{
+                this.remove(name);
+                this.keyframesRule['appendRule' in this.keyframesRule?'':'insertRule'](name+' {'+getKeyframesRule(value)+'}');
+            }
+            
+            return this.extract();
+        },
+        remove:function(name){
+            this.keyframesRule.deleteRule(fixKey(name));
+            return this.extract();
+        },
+        clear:function(){
+            slice.call(this.cssRules).forEach(function(rule){
+                this.remove(rule.keyText);
+            }.bind(this));
+            return this.extract();
+        }
+    }
+
+            
+    var extend={
+        vendor:cssVendor,
+        get:function(name){
+            return this.CSSKeyframes[name];
+        },
+        add:function(){
+            return this.apply(null,arguments);
+        },
+        remove:function(name){
+            iterateSheet(function(rule,i,sheet){
+                if(rule.name==(name.name||name))
+                    sheet.deleteRule(i);
+            });
+            return true;
+        },
+        animation:camelCase(fixCSS3('animation')),
+        support:camelCase('animation') in divstyle || camelCase(cssVendor+'animation') in divstyle
+    }
+
+    if(typeof Object.defineProperties=='function'){
+        
+        Object.keys(extend).forEach(function(name){
+            struct[name]=extend[name];
+        });
+
+        "name cssText cssRules".split(" ").forEach(function(prop){
+            Object.defineProperty(struct.prototype,prop,{
+                get:function(){
+                    return this.keyframesRule[prop];
+                },
+                enumerable:true
+            });
+        });
+
+        Object.defineProperty(struct,'CSSKeyframes',{
+            get:function(){
+                var ret={};
+                iterateSheet(function(rule){
+                    ret[rule.name]=this(rule);
+                }.bind(this));
+                return ret;
+            },
+            enumerable:true
+        });
+
+    }
+
+    ROOT.JS2CSSKeyframes=struct;
+    
+})(window, function(name, keys){
+    if(!(this instanceof arguments.callee)){
+        return new arguments.callee(name, keys);
+    }
+    this.init(name, keys);
+});
